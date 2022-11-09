15475cd983deddbf09cca0b23d82e25743243d4f
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-11-09 16:25:07 -0500

    created Navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
+import Navbar from "./components/Navbar";
 
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello World!
-      </h1>
+      <Navbar />
     </div>
   );
 }
