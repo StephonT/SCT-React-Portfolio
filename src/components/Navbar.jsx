2cf7042cf3d7c98d12e90adfae30699328e12e5d
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-11-10 22:19:50 -0500

    added styling to social icons div

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
       </ul>
 
       {/* social icons */}
-      <div className="flex fixed flex-col top-[35%] left-0">
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
         <ul>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
             <a className="flex justify-between items-center w-full text-gray-300" href="https://www.linkedin.com/in/stephon-treadwell/" target="_blank">
@@ -68,3 +68,4 @@ const Navbar = () => {
 
 export default Navbar;
 
+// left off at 32:42
