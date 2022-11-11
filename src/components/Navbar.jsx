c835d1711726bb77f406a31837390e8b30b189f5
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-11-10 22:07:23 -0500

    added social icons with links on navbar page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
-
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import {BsFillPersonLinesFill} from 'react-icons/bs'
 
 const Navbar = () => {
 
@@ -36,9 +37,33 @@ const Navbar = () => {
       </ul>
 
       {/* social icons */}
-      <div className="hidden"></div>
+      <div className="flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
+            <a className="flex justify-between items-center w-full text-gray-300" href="https://www.linkedin.com/in/stephon-treadwell/" target="_blank">
+              Linkedin <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-black">
+            <a className="flex justify-between items-center w-full text-gray-300" href="https://github.com/StephonT" target="_blank">
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#b91c1c]">
+            <a className="flex justify-between items-center w-full text-gray-300" href= "mailto: streadwe@gmail.com" target="_blank">
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#475569]">
+            <a className="flex justify-between items-center w-full text-gray-300" href="https://github.com/StephonT" target="_blank">
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
 
 export default Navbar;
+
