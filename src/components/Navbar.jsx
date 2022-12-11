45f1c1647dc313d9594a38d2d947da2654daf74c
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-12-11 11:07:09 -0500

    changed alt in attribute to not use redundant code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-amber-300 text-gray-300">
       <div>
-        <img src={Logo} alt="Logo Image" style={{ width: "100px", marginTop: "15px" }} />
+        <img src={Logo} alt="/" style={{ width: "100px", marginTop: "15px" }} />
       </div>
 
       {/* menu */}
